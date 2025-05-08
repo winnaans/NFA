7c0b0edf70d1dada0bcaf4db938b86e1fa7e391b
winnaans <apriliawinna183@gmail.com>
2025-05-08 17:47:49 +0700

    upload tugas 2 laravel

--- Pertemuan 2/booksales_api/resources/views/welcome.blade.php
+++ Pertemuan 2/booksales_api/resources/views/welcome.blade.php
@@ -19,7 +19,6 @@
             </style>
         @endif
     </head>
-    
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
         <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 not-has-[nav]:hidden">
             @if (Route::has('login'))
